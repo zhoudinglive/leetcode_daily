2b210d627f4a64ba1928006044d48a90f6185e4d
zhouding <zhoudinglive@gmail.com>
2018-12-12 11:54:53 +0800

    20181212

---
+++ array/905_sort_array_by_parity.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+#include <algorithm>
+#include <vector>
+using namespace std;
+
+bool cmp(int a, int b) {
+	return (a % 2) < (b % 2);
+}
+
+class Solution {
+public:
+	vector<int> sortArrayByParity(vector<int>& A) {	
+		sort(A.begin(), A.end(), cmp);
+        return A;
+	}
+};
