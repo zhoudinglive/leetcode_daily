ea54acb314739be57daf4f774c089bba5912f113
zhouding <zhoudinglive@gmail.com>
2018-12-24 21:38:20 +0800

    20181224

---
+++ array/448_find_all_numbers_disappeared_in_an_array.cpp
@@ -0,0 +1,23 @@
+#include<iostream>
+#include<algorithm>
+#include<vector>
+using namespace std;
+
+class Solution {
+  public:
+    vector<int> findDisappearedNumbers(vector<int> &nums) {
+        vector<int> ans;
+        for(int i = 0; i < nums.size(); ++i) {
+            int val = abs(nums[i]) - 1;
+            if(val > 0) {
+                nums[val] = -nums[val];
+            }
+        }
+        for(int i = 0; i < nums.size(); ++i) {
+            if(nums[i] > 0) {
+                ans.push_back(i + 1);
+            }
+        }
+        return ans;
+    }
+};
