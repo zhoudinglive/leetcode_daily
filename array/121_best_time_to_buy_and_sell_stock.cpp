9a8eec414e88dc96319c1ec5ad41c7fa103bc3dc
zhouding <zhoudinglive@gmail.com>
2019-01-20 23:05:54 +0800

    20180120

---
+++ array/121_best_time_to_buy_and_sell_stock.cpp
@@ -0,0 +1,20 @@
+#include<iostream>
+#include<algorithm>
+#include<vector>
+using namespace std;
+
+class Solution {
+  public:
+    int maxProfit(vector<int> &prices) {
+        int profit = 0;
+        for(int i = 0; i < prices.size(); ++i) {
+            for(int j = i; j < prices.size(); ++j) {
+                if(prices[j] > prices[i]) {
+                    int div = prices[j] - prices[i];
+                    profit = div > profit ? div : profit;
+                }
+            }
+        }
+        return profit;
+    }
+};
