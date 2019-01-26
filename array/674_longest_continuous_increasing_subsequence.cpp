584c8f1ce949d482da4fa6b80c31b773d773e446
zhouding <zhoudinglive@gmail.com>
2019-01-26 22:56:31 +0800

    20190126

---
+++ array/674_longest_continuous_increasing_subsequence.cpp
@@ -0,0 +1,26 @@
+#include<iostream>
+#include<algorithm>
+#include<vector>
+using namespace std;
+#define maxn 10005
+
+class Solution {
+  public:
+    int findLengthOfLCIS(vector<int> &nums) {
+        int dp[maxn];
+        for(int i = 0; i < nums.size(); ++i) {
+            dp[i] = 1;
+        }
+        for(int i = 1; i < nums.size(); ++i) {
+            if(nums[i] > nums[i - 1]) {
+                dp[i] = dp[i - 1] + 1;
+            }
+        }
+
+        int ans = -1;
+        for(int i = 0; i < nums.size(); ++i) {
+            ans = max(ans, dp[i]);
+        }
+        return ans;
+    }
+};
