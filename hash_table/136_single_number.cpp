9dad86e3405086fe15cfc601b52d6865cf46008a
zhouding <zhoudinglive@gmail.com>
2019-02-22 12:03:27 +0800

    20190222

---
+++ hash_table/136_single_number.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+#include<algorithm>
+#include<vector>
+using namespace std;
+
+class Solution {
+  public:
+    int singleNumber(vector<int> &nums) {
+        int ans = 0;
+        for(int i = 0; i < nums.size(); ++i) {
+            ans ^= nums[i];
+        }
+        return ans;
+    }
+};
