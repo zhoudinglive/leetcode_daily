36bf622d2c2efec8f0f072b0425d72bc22f0c7e0
zhouding <zhoudinglive@gmail.com>
2019-03-06 22:49:28 +0800

    20190306

---
+++ hash_table/1002_find_common_characters.py
@@ -0,0 +1,6 @@
+class Solution:
+    def commonChars(self, A: List[str]) -> List[str]:
+        res = collections.Counter(A[0])
+        for a in A:
+            res &= collections.Counter(a)
+        return list(res.elements())
