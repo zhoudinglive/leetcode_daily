a375669ba5e67751293fd49668fdcdf14bc6c8b3
周丁 <carpenter@zhoudingdeMacBook-Pro.local>
2019-11-04 10:58:40 +0800

    20191104

---
+++ linked_list/83_Remove_Duplicates_from_Sorted_List.java
@@ -0,0 +1,22 @@
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode(int x) { val = x; }
+ * }
+ */
+class Solution {
+    public ListNode deleteDuplicates(ListNode head) {
+        ListNode temp = head;
+
+        while(temp != null) {
+            if(temp.next != null && temp.val == temp.next.val) {
+                temp.next = temp.next.next;
+            } else {
+                temp = temp.next;
+            }
+        }
+        return head;
+    }
+}
